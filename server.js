07217fdb35911725f2e6899371e9bdfa116d0aad
mreiben <jason.eiben@gmail.com>
2017-01-07 22:09:48 +0000

    fixed port for heroku deployment

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
-app.listen(8080, function () {
-  console.log('Listening on port 8080!');
+var server_port = process.env.YOUR_PORT || process.env.PORT || 8080;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, function() {
+    console.log('Listening on port %d', server_port);
 });
